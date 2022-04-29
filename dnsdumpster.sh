0fc071c4749a5b27558ee3d0d7c705e37323ffe6
Rob VandenBrink <rob@coherentsecurity.com>
2022-04-29 09:00:47 -0400

    Update dnsdumpster.sh

--- dnsdumpster.sh
+++ dnsdumpster.sh
@@ -1,4 +1,4 @@
 # this service will allow low volume queries without a key, this is the version below
 # for higher volume queries and full results, get an API key and reverse the two lines below
-curl -s -k https://api.hackertarget.com/reverseiplookup/?q=$1
+curl -s -k https://api.hackertarget.com/reverseiplookup/?q=$1 | tee $1.dnsdumpster.txt
 # curl -s -k https://api.hackertarget.com/reverseiplookup/?q=$1&apikey=<API KEY>
