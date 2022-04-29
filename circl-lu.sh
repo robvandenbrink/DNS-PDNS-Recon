a005bc81e4de6df8d2ba819b11a62ca136514199
Rob VandenBrink <rob@coherentsecurity.com>
2022-04-29 09:00:16 -0400

    Update circl-lu.sh

--- circl-lu.sh
+++ circl-lu.sh
@@ -1,1 +1,1 @@
-curl -s -k -u <userid>:<API Key> -X GET "https://www.circl.lu/pdns/query/$1" -H "accept: application/json" | jq
+curl -s -k -u <userid>:<API Key> -X GET "https://www.circl.lu/pdns/query/$1" -H "accept: application/json" | jq | tee $1.circl.txt
